36b0fc823b7405ca9a00064bff871943b6d17e01
Magnus Borgstrom <magnub@gmail.com>
2013-05-21 12:03:35 +0200

    Initiating handlers on background thread and one findbugs issue
    * The handlers were created on main thread, not needed here, they are
    now running in separate handler threads
    
    * Fixed one findbugs issue with mac adress doing string comparision with
    ==

--- src/com/sensorcon/sdhelper/SDHelper.java
+++ src/com/sensorcon/sdhelper/SDHelper.java
@@ -17,6 +17,7 @@ import android.content.DialogInterface.OnDismissListener;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Color;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -27,13 +28,13 @@ import android.widget.AdapterView.OnItemClickListener;
 /**
  * This is a class of helpful methods to do some things with your Sensordrone.
  * 
- * It is intended to be helpful, and get your projects up and running quickly, but
- * is not part of the official Sensordrone API. Your mileage may vary :-)
+ * It is intended to be helpful, and get your projects up and running quickly,
+ * but is not part of the official Sensordrone API. Your mileage may vary :-)
  * 
  * This is built against Android-10 (Gingerbread).
  * 
  * @author Mark Rudolph, Sensorcon, Inc.
- *
+ * 
  */
 public class SDHelper {
 
@@ -43,15 +44,20 @@ public class SDHelper {
 	private IntentFilter btFilter;
 	private Dialog scanDialog; // The Dialog we will display results in
 	private AlertDialog.Builder dBuilder; // A builder for the Dialog
-	
+
 	/**
 	 * We will use this to scan for MAC addresses
-	 *
-	 * @param drone The Drone to connect
-	 * @param context Your context (for displaying a Dialog)
-	 * @param includePairedDevices True if you want to include the phone's paired devices in the list
+	 * 
+	 * @param drone
+	 *            The Drone to connect
+	 * @param context
+	 *            Your context (for displaying a Dialog)
+	 * @param includePairedDevices
+	 *            True if you want to include the phone's paired devices in the
+	 *            list
 	 */
-	public void scanToConnect(final Drone drone, final Activity activity, final Context context, boolean includePairedDevices) {
+	public void scanToConnect(final Drone drone, final Activity activity,
+			final Context context, boolean includePairedDevices) {
 
 		// Set up our Bluetooth Adapter
 		btAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -81,28 +87,25 @@ public class SDHelper {
 
 		// Set up our ArrayAdapter
 		final ArrayAdapter<String> macAdapter = new ArrayAdapter<String>(
-				context,
-				android.R.layout.simple_list_item_1);
+				context, android.R.layout.simple_list_item_1);
 		macList.setAdapter(macAdapter);
-
-
 
 		// Add in the paired devices if asked
 		if (includePairedDevices) {
 			// Get the list of paired devices
 			Set<BluetoothDevice> pairedDevices = btAdapter.getBondedDevices();
 			// Add paired devices to the list
-			if (pairedDevices.size() > 0){
-				for (BluetoothDevice device : pairedDevices){
+			if (pairedDevices.size() > 0) {
+				for (BluetoothDevice device : pairedDevices) {
 					// We only want Sensordrones
-					if (device.getName().contains("drone")){
+					if (device.getName().contains("drone")) {
 						// Add the Name and MAC
-						macAdapter.add(device.getName() + "\n" + device.getAddress());
+						macAdapter.add(device.getName() + "\n"
+								+ device.getAddress());
 					}
 				}
 			}
 		}
-
 
 		// What to do when we find a device
 		btReceiver = new BroadcastReceiver() {
@@ -113,20 +116,25 @@ public class SDHelper {
 				// When discovery finds a device
 				if (BluetoothDevice.ACTION_FOUND.equals(action)) {
 					// Get the BluetoothDevice object from the Intent
-					BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+					BluetoothDevice device = intent
+							.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
 					// We only want Sensordrones
 					try {
-						if (device.getName().contains("drone")){
+						if (device.getName().contains("drone")) {
 							// Add the Name and MAC
-							macAdapter.add(device.getName() + "\n" + device.getAddress());
-						} 
+							macAdapter.add(device.getName() + "\n"
+									+ device.getAddress());
+						}
 					} catch (NullPointerException n) {
-						// Some times getName() will return null, which doesn't parse very well :-)
+						// Some times getName() will return null, which doesn't
+						// parse very well :-)
 						// Catch it here
 						Log.d("SDHelper", "Found MAC will null string");
-						// You can still add it to the list if you want, it just might not
+						// You can still add it to the list if you want, it just
+						// might not
 						// be a Sensordrone...
-						//macAdapter.add("nullDevice" + "\n" + device.getAddress());
+						// macAdapter.add("nullDevice" + "\n" +
+						// device.getAddress());
 					}
 				}
 			}
@@ -136,7 +144,6 @@ public class SDHelper {
 		btFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
 		context.registerReceiver(btReceiver, btFilter);
 		// Don't forget to unregister when done!
-
 
 		// Start scanning for Bluetooth devices
 		btAdapter.startDiscovery();
@@ -150,8 +157,8 @@ public class SDHelper {
 		macList.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
 
 				// Once an item is selected...
 
@@ -163,7 +170,7 @@ public class SDHelper {
 				// Get the MAC address
 				MAC = macAdapter.getItem(arg2);
 				int MACLength = MAC.length();
-				MAC = MAC.substring(MACLength-17, MACLength);
+				MAC = MAC.substring(MACLength - 17, MACLength);
 
 				// Dismiss the dialog
 				scanDialog.dismiss();
@@ -180,25 +187,32 @@ public class SDHelper {
 			public void onDismiss(DialogInterface arg0) {
 				// Don't connect if the MAC is not set
 				// This protects against trying to connect if the user cancels
-				if (MAC != "") {
+				if (!TextUtils.isEmpty(MAC)) {
 					// Display a message if the connect fails
 					if (!drone.btConnect(MAC)) {
 						activity.runOnUiThread(new Runnable() {
 							public void run() {
-								AlertDialog.Builder alert = new AlertDialog.Builder(context);
+								AlertDialog.Builder alert = new AlertDialog.Builder(
+										context);
 								alert.setTitle("Couldn't connect");
 								alert.setMessage("Connection was not successful.\nPlease try again!");
-								alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-									public void onClick(DialogInterface dialog, int whichButton) {
-										// If you wanted to try again directly, you could add that
-										// here (and add a "cancel button" to not scan again).
-									}
-								});
-								
+								alert.setPositiveButton("Ok",
+										new DialogInterface.OnClickListener() {
+											public void onClick(
+													DialogInterface dialog,
+													int whichButton) {
+												// If you wanted to try again
+												// directly, you could add that
+												// here (and add a
+												// "cancel button" to not scan
+												// again).
+											}
+										});
+
 								alert.show();
 							}
 						});
-					
+
 					}
 				}
 
@@ -226,7 +240,8 @@ public class SDHelper {
 	 */
 	public boolean isBTEnabled(Context context, BluetoothAdapter btAdapter) {
 		if (!btAdapter.isEnabled()) {
-			Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			Intent enableBtIntent = new Intent(
+					BluetoothAdapter.ACTION_REQUEST_ENABLE);
 			context.startActivity(enableBtIntent);
 			return false;
 		} else {
@@ -237,18 +252,26 @@ public class SDHelper {
 	/**
 	 * A function to flash the LEDs
 	 * 
-	 * Note There are Thread.sleep() calls for delay in between flashing. If you call
-	 * this immediately before disconnecting, all of the blinks might not be processed!
+	 * Note There are Thread.sleep() calls for delay in between flashing. If you
+	 * call this immediately before disconnecting, all of the blinks might not
+	 * be processed!
 	 * 
-	 * @param drone The Drone
-	 * @param nFlashes How many times do we flash?
-	 * @param delay_ms Delay between flashes in ms
-	 * @param Red 0-255
-	 * @param Green 0-255
-	 * @param Blue 0-255
+	 * @param drone
+	 *            The Drone
+	 * @param nFlashes
+	 *            How many times do we flash?
+	 * @param delay_ms
+	 *            Delay between flashes in ms
+	 * @param Red
+	 *            0-255
+	 * @param Green
+	 *            0-255
+	 * @param Blue
+	 *            0-255
 	 */
-	public void flashLEDs(Drone drone, int nFlashes, int delay_ms, int Red, int Green, int Blue) {
-		for (int i=0; i < nFlashes; i++) {
+	public void flashLEDs(Drone drone, int nFlashes, int delay_ms, int Red,
+			int Green, int Blue) {
+		for (int i = 0; i < nFlashes; i++) {
 			drone.setRightLED(0, 0, 0);
 			drone.setLeftLED(Red, Green, Blue);
 			try {
@@ -263,7 +286,7 @@ public class SDHelper {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			if (i == nFlashes-1) {
+			if (i == nFlashes - 1) {
 				drone.setLEDs(0, 0, 0);
 				try {
 					Thread.sleep(delay_ms);
@@ -274,10 +297,6 @@ public class SDHelper {
 		}
 	}
 
-
-
-
-
 	/**
 	 * Our default constructor
 	 */
@@ -286,4 +305,3 @@ public class SDHelper {
 	}
 
 }
-
